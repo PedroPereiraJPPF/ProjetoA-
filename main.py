d0e5e91ac7dae04943f250c780c828c633b918f5
Lucas <lugab037@gmail.com>
2025-02-14 20:39:26 -0300

    update: adiciona novo exemplo de grafo utilizando cidades.

--- main.py
+++ main.py
@@ -121,8 +121,8 @@ for index in range(len(vertices)):
 
 ########## ALGORITMO A* ##########
 
-label_start = 'A'
-label_end = 'D'
+label_start = 'BoaVista'
+label_end = 'Aracaju'
 index_inicial = vertices.index(label_start)
 index_final = vertices.index(label_end)
 open_list = [{
